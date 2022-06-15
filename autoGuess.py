8507473b88a09791f17bd441e3730c45a9281f00
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2022-06-14 21:28:36 -0500

    added user deciding the numbers and comp keeps guessing

--- autoGuess.py
+++ autoGuess.py
@@ -1,12 +1,13 @@
 import random
 
-def autoGuess(x,y):
-    magicNum = random.randint(x,y)
-    guess = random.randint(x, y)
-    while guess != magicNum :
-        guess = random.randint(x, y)
-        print('Not there yet!')
-    print('The computer figured it out')
-    
+def autoGuess():
+    firstNum = int(input('What should be the lower number be? '))
+    secNum = int(input('What should the highest number be? '))
+    magicNum = random.randint(firstNum,secNum)
+    guess = random.randint(firstNum,secNum)
+    while guess != magicNum:
+        guess = random.randint(firstNum,secNum)
+        print('Missed the mark, keep trying')
+    print('Nice job!')
 
-autoGuess(1,5)
+autoGuess()
