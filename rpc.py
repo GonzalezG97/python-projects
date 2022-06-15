5b62d6d0a87b0a2521c13bc23ea99731b443197e
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2022-06-14 21:50:50 -0500

    Added a win condition func

--- rpc.py
+++ rpc.py
@@ -1,6 +1,15 @@
 import random
 
 def play():
-    user = input('Rock(R), Paper(P), Scissors(S)').lower()
+    user = input('Rock(R), Paper(P), Scissors(S) ').lower()
     comp = random.choice(['r', 'p', 's'])
-    
+    if user == comp:
+        return "Its a tie"
+    if winCon(user, comp):
+        return "Victory for you and your family!"
+
+def winCon(user, comp):
+    if (user == 'r' and comp == 's') or (user == 'p' and comp == 'r') or (user == 's' and comp == 'p'):
+        return True
+
+play()
