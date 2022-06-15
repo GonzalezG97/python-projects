36aebbe654fec078a191acef1fd5006bc8f5e8ef
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2022-06-14 21:19:48 -0500

    Added a check to keep the computer guessing

--- autoGuess.py
+++ autoGuess.py
@@ -3,7 +3,10 @@ import random
 def autoGuess(x,y):
     magicNum = random.randint(x,y)
     guess = random.randint(x, y)
-    print(magicNum)
-    print(guess)
+    while guess != magicNum :
+        guess = random.randint(x, y)
+        print('Not there yet!')
+    print('The computer figured it out')
+    
 
 autoGuess(1,5)
